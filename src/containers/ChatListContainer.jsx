8c3e99a024f874861bf91b6495fc6201785cc6d4
Nerio Balza <neriojbd@gmail.com>
2022-08-08 23:26:16 -0400

    Working on the ChatList Container

--- src/containers/ChatListContainer.jsx
+++ src/containers/ChatListContainer.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 
+import "../assets/styles/containers/ChatListContainer.scss";
+import { BiPencil, BiMenu } from "react-icons/bi";
+
 const ChatListContainer = () => {
   return (
     <section className="chatlist-container">
       <div className="chatlist">
-        <header className="chatlist__header"></header>
+        <header className="chatlist__header">
+          <button>
+            <BiMenu />
+          </button>
+          <input type="text" />
+        </header>
         <ul className="chatlist__list"></ul>
       </div>
 
-      <div className="cotactlist-bottom">
+      <div className="cotactlist">
         <ul className="contactlist__list"></ul>
       </div>
+
+      <button className="new-chat">
+        <BiPencil />
+      </button>
     </section>
   );
 };
