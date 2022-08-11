255180c5184161bbaba44da5de56aabc20c6f62f
Nerio Balza <neriojbd@gmail.com>
2022-08-10 22:16:28 -0400

    Main menu layout

--- src/components/ChatListHeader.jsx
+++ src/components/ChatListHeader.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "../assets/styles/components/ChatListHeader.scss";
 
 import InputSearch from "../components/InputSearch";
-import { BiMenu } from "react-icons/bi";
+import {
+  BiMenu,
+  BiBookmark,
+  BiUser,
+  BiCog,
+  BiMoon,
+  BiMeteor,
+  BiHelpCircle,
+  BiBug,
+} from "react-icons/bi";
 
 const ChatListHeader = () => {
   return (
@@ -10,6 +19,40 @@ const ChatListHeader = () => {
       <button className="menu-button">
         <BiMenu />
       </button>
+
+      <nav className="main-menu">
+        <ul className="main-menu__list">
+          <li className="main-menu__list--item">
+            <BiBookmark />
+            <p>Saved Messages</p>
+          </li>
+          <li className="main-menu__list--item">
+            <BiUser />
+            <p>Contacts</p>
+          </li>
+          <li className="main-menu__list--item">
+            <BiCog />
+            <p>Settings</p>
+          </li>
+          <li className="main-menu__list--item">
+            <BiMoon />
+            <p>Dark Mode</p>
+          </li>
+          <li className="main-menu__list--item">
+            <BiMeteor />
+            <p>Animations</p>
+          </li>
+          <li className="main-menu__list--item">
+            <BiHelpCircle />
+            <p>ChatApp Features</p>
+          </li>
+          <li className="main-menu__list--item">
+            <BiBug />
+            <p>Report Bug</p>
+          </li>
+        </ul>
+        <p className="main-menu__app-version">ChatApp WebK 1.0.0</p>
+      </nav>
 
       <InputSearch id="input-search" name="input-search" placeholder="Search" />
     </header>
